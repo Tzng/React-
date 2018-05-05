88fd522e564d44e085385d538bb18675c4a01464
bin.tang <569693942@qq.com>
2018-05-05 18:49:26 +0800

    引入路由

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BasicForm from './components/form/BasicForm'
 import './index.css';
+import {Router, Route, hashHistory, IndexRedirect} from 'react-router'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    // 引入路由
+    <Router history={hashHistory}>
+        <Route path={'app'} components={App}></Route>
+        <Route path={'form'} components={BasicForm}/>
+    </Router>,
+    document.getElementById('root')
 );
