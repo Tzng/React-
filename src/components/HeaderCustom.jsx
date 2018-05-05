ba2ebdbe6baff293d00c545b354c2e2e8d9ab9cb
bin.tang <569693942@qq.com>
2018-05-05 16:37:49 +0800

    一些代码规范警告

--- src/components/HeaderCustom.jsx
+++ src/components/HeaderCustom.jsx
@@ -20,21 +20,21 @@ class HeaderCustom extends Component {
                     mode="horizontal" //水平模式
                     style={{lineHeght: '64px', float: 'right'}}
                 >
-                    <Menu.Item key='1'>
+                    <Menu.Item key="1">
                         {/*设置位置*/}
                         <Badge count={25} overflow={10} style={{marginLeft: 10}}>
-                            <Icon type="notification"/>
+                            <Icon type="notification" />
                         </Badge>
                     </Menu.Item>
                     {/*菜单子菜单*/}
-                    <SubMenu title={<span><Icon type={"user"}/>你好，admin</span>}>
+                    <SubMenu title={<span><Icon type={"user"} />你好，admin</span>}>
                         <MenuItemGroup title="用户中心">
                             <Menu.Item key="setting:1">个人信息</Menu.Item>
                         </MenuItemGroup>
-                        <MenuItemGroup titkle="设置中心">
+                        <MenuItemGroup title="设置中心">
                             {/*添加单个小菜单*/}
-                            <Menu.Item key = "setting:3">个人设置</Menu.Item>
-                            <Menu.Item key = "setting:4">系统设置</Menu.Item>
+                            <Menu.Item key="setting:3">个人设置</Menu.Item>
+                            <Menu.Item key="setting:4">系统设置</Menu.Item>
                         </MenuItemGroup>
                     </SubMenu>
                 </Menu>
