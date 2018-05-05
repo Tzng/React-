bccbe63a40ac735563726e2e2a68f5a3944026f4
allahbin <569693942@qq.com>
2018-05-06 00:08:17 +0800

    表格的添加

--- src/components/SiderCustom.jsx
+++ src/components/SiderCustom.jsx
@@ -71,8 +71,8 @@ class SiderCustom extends Component{
                         key={"/app/table"}
                         title={<span><Icon type={"copy"} /><span className={"nav-text"}>表格</span></span>}
                     >
-                        <Menu.Item key="/app/table/advancedTable"><Link to={'/app/table/advancedTable'}>高级表格</Link></Menu.Item>
-                        <Menu.Item key="/app/table/basicTable"><Link to={'/app/table/basicTable'}>基础表格</Link></Menu.Item>
+                        <Menu.Item key="/app/table/advancedtables"><Link to={'/app/table/advancedtables'}>高级表格</Link></Menu.Item>
+                        <Menu.Item key="/app/table/basictables"><Link to={'/app/table/basictables'}>基础表格</Link></Menu.Item>
                     </SubMenu>
                     <SubMenu
                         key={"/app/form"}
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import {Router, Route, hashHistory, IndexRedirect} from 'react-router'
 
 //自有组件
-import BasicForm from './components/form/BasicForm';
-import WrappedForm from './components/form/WrappedForm';
+import BasicForm from './components/forms/BasicForm';
+import WrappedForm from './components/forms/WrappedForm';
 import Page from './components/Page'
 import './index.css';
+import BasicTables from "./components/tables/BasicTables";
+import AdvancedTables from "./components/tables/AdvancedTables";
 
 ReactDOM.render(
     // 引入路由
@@ -16,11 +18,18 @@ ReactDOM.render(
         {/*默认路由,访问根路径的时候，这里的子组件都能渲染*/}
         <Route path={'/'} components={Page}>
             {/*默认路由，这是Route3版本的写法，4是另一种写法了*/}
-            <IndexRedirect to={'/app'}/>
+            <IndexRedirect to={'app'}/>
             <Route path={'app'} components={App}>
-                {/*嵌套路由，子路由渲染，也就是说，在Content中需要出现我们的其他组件*/}
-                <Route path={'form/basicform'} components={BasicForm}/>
-                <Route path={'form/wrappedform'} components={WrappedForm}/>
+                <Route path={'form'}>
+                    {/*嵌套路由，子路由渲染，也就是说，在Content中需要出现我们的其他组件*/}
+                    <Route path={'basicform'} components={BasicForm}/>
+                    <Route path={'wrappedform'} components={WrappedForm}/>
+                </Route>
+                {/*表格路由*/}
+                <Router path={'table'}>
+                    <Route path={'basictables'} component={BasicTables}/>
+                    <Route path={'advancedtables'} component={AdvancedTables}/>
+                </Router>
             </Route>
         </Route>
 
